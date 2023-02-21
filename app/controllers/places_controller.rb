2c6db0db4d9b7b617c7d1102c5968448b93a02b0
ursulacalmet <ursula.calmet@kellogg.northwestern.edu>
2023-02-21 17:47:59 +0000

    changes

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,31 +1,29 @@
-        class PlacesController < ApplicationController
-            def index
-              @places = Place.all
-            end
-          
-            def show
-              @place = Place.find(params[:id])
-              @posts = @place.posts
-            end
-          
-            def new
-              @place = Place.new
-            end
-          
-            def create
-              @place = Place.new(place_params)
-          
-              if @place.save
-                redirect_to place_path(@place)
-              else
-                render :new
-              end
-            end
-          
-            private
-          
-            def place_params
-              params.require(:place).permit(:name)
-            end
-          end
-          
+class PlacesController < ApplicationController
+  def index
+    @places = Place.all
+  end
+
+  def show
+    @place = Place.find(params[:id])
+    @posts = @place.posts
+  end
+
+  def new
+    @place = Place.new
+  end
+
+  def create
+    @place = Place.new(place_params)
+    if @place.save
+      redirect_to places_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def place_params
+    params.require(:place).permit(:name)
+  end
+end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,25 +1,22 @@
 class PostsController < ApplicationController
-    def new
-      @place = Place.find(params[:place_id])
-      @post = Post.new
-    end
-  
-    def create
-      @place = Place.find(params[:place_id])
-      @post = @place.posts.create(post_params)
+  def new
+    @place = Place.find(params[:place_id])
+    @post = @place.posts.new
+  end
+
+  def create
+    @place = Place.find(params[:place_id])
+    @post = @place.posts.new(post_params)
+    if @post.save
       redirect_to place_path(@place)
-    end
-  
-    def show
-        @post = Post.find(params[:id])
-      end
-      
-    private
-  
-    def post_params
-      params.require(:post).permit(:title, :description, :posted_on)
+    else
+      render :new
     end
   end
 
+  private
 
-  
+  def post_params
+    params.require(:post).permit(:title, :description, :posted_on)
+  end
+end
